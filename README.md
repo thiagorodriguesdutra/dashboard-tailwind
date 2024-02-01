80ec253ef5cd0fe6187dd981141d53e29059c25a
thiagorodriguesdutra <thiagorsd@icloud.com>
2024-02-01 12:29:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### Meu Website
+
+Este é meu futuro website/portfolio.
+
+E essas são as features que serão desenvolvidas:
+
+[ ] - Dark Mode
+[ ] - Estrutura da página "Sobre Mim"
+[ ] - Estrutura da página "Meus Projetos"
+[ ] - Estrutura da página "Blog"
+[ ] - Integração com a API do Github
+[ ] - Newsletter
+[ ] - Criar blog com mdx
+
+
+Live at: em breve...
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Dashboard | Tailwind',
+  description: 'Dashboard | Tailwind',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${inter.className} h-screen`}>{children}</body>
+    </html>
+  )
+}
